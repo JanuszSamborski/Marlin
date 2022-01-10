bd1dbd06e065464753d086647df95b2f62a12c7a
JanuszSamborski <janusz.joe.samborski@gmail.com>
2022-01-10 20:19:09 +0100

    chore: changed safe homing location

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1755,7 +1755,7 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
+  #define Z_SAFE_HOMING_X_POINT 10  // X point for Z homing
   #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // Y point for Z homing
 #endif
 
